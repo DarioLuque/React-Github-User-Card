5eedb4ed15d58bc244f3178331d7330a4e83a9aa
Dario Luque <dario.lu92@gmail.com>
2020-04-10 17:51:01 -0700

    data retrieved for user, starting userCard

--- react-github-usercard/src/App.js
+++ react-github-usercard/src/App.js
@@ -3,12 +3,22 @@ import axios from 'axios';
 
 
 class App extends React.Component {
-  
+  state = {
+    user: {}
+  };
+
+  componentDidMount() {
+    axios.get('https://api.github.com/users/DarioLuque').then(res => {
+      console.log(res.data, "user response.data")
+      this.setState({user: Response.data});
+    });
+  }
+
   render(){
   return (
 
     <div>
-
+      
     </div>
   );
   }
